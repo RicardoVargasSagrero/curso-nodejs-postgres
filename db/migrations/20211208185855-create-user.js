05b82a34d7be8df46fd7a7c10789fafac3d63cdb
Ricardo Vargas Sagrero <r.sagrero1@gmail.com>
2022-01-20 17:03:15 -0600

    Cambio en las migraciones, se tenía problemas poruque los schemas de los modelos ya estaban actualziados a la manera adecuada, en las migraciones hubieron tablas a la cuales solo les cambiamos un tipo de propiedades

--- db/migrations/20211208185855-create-user.js
+++ db/migrations/20211208185855-create-user.js
@@ -2,11 +2,34 @@
 /**
  * MODEL CREATION
  */
-const { UserSchema, USER_TABLE } = require('../models/user.model');
+const { USER_TABLE } = require('../models/user.model');
+const { DataTypes, Sequelize } = require('sequelize');
 
 module.exports = {
   up: async (queryInterface /**, Sequelize*/) => {
-    await queryInterface.createTable(USER_TABLE, UserSchema);
+    await queryInterface.createTable(USER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'create_at',
+        defaultValue: Sequelize.NOW,
+      }
+    });
   },
 
   down: async (queryInterface /*, Sequelize*/) => {
--- db/migrations/20220113231520-create-customers.js
+++ db/migrations/20220113231520-create-customers.js
@@ -1,10 +1,48 @@
 'use strict';
 
-const { CustomerSchema, CUSTOMER_TABLE } = require('../models/customer.model');
+const { CUSTOMER_TABLE } = require('../models/customer.model');
+const {USER_TABLE} = require('./../models/user.model');
+const { DataTypes, Sequelize} = require('sequelize');
 
 module.exports = {
   up: async (queryInterface /**, Sequelize*/) => {
-    await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema);
+    await queryInterface.createTable(CUSTOMER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      lastName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      phone: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+      userId: {
+        fiel: 'user_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: USER_TABLE,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+    });
   },
 
   down: async (queryInterface /*, Sequelize*/) => {
