d347902890cc2fd2c20a6a5d8baaf292ef121e16
Ricardo Vargas Sagrero <r.sagrero1@gmail.com>
2022-01-20 16:37:21 -0600

    Cambio de la configuración con dialectOptions

--- db/config.js
+++ db/config.js
@@ -12,8 +12,10 @@ module.exports = {
   production: {
     url: config.dbURL,
     dialect: 'postgres',
-    ssl: {
-      rejectUnauthorized: false,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
   },
 };
