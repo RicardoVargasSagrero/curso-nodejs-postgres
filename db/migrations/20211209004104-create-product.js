53f97ca19ec668ef44b41862d5151093544e303a
Ricardo Vargas Sagrero <r.sagrero1@gmail.com>
2022-01-21 01:56:51 -0600

    No se generaba la tabla de categorias

--- db/migrations/20211209004104-create-product.js
+++ db/migrations/20211209004104-create-product.js
@@ -1,12 +1,15 @@
 'use strict';
 
-const {ProductSchema, PRODUCTS_TABLE} = require('../models/product.model');
+const { CATEGORY_TABLE, CategorySchema } = require('../models/category.model');
+const { ProductSchema, PRODUCTS_TABLE } = require('../models/product.model');
 module.exports = {
-  up: async (queryInterface, /*Sequelize*/) => {
-    await queryInterface.createTable(PRODUCTS_TABLE,ProductSchema)
+  up: async (queryInterface /*Sequelize*/) => {
+    await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
+    await queryInterface.createTable(PRODUCTS_TABLE, ProductSchema);
   },
 
-  down: async (queryInterface, /*Sequelize*/) => {
+  down: async (queryInterface /*Sequelize*/) => {
+    await queryInterface.dropTable(CATEGORY_TABLE);
     await queryInterface.dropTable(PRODUCTS_TABLE);
-  }
+  },
 };
