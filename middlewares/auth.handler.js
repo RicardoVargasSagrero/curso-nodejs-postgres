4ba62482d1c7016bb75bab0fbbdfc3c1e3b7f4e6
Ricardo Vargas Sagrero <r.sagrero1@gmail.com>
2022-01-24 12:15:59 -0600

    Se trabajo en la autenticación y control de roles para el ENDPOINT de categorias, falta la gestión en todos los ENDPOINTS dependiendo los casos de uso

--- middlewares/auth.handler.js
+++ middlewares/auth.handler.js
@@ -1,5 +1,5 @@
 const boom = require('@hapi/boom');
-const {config} = require('./../config/config');
+const { config } = require('./../config/config');
 
 function checkApiKey(req, res, next) {
   const apiKey = req.headers['api'];
@@ -9,5 +9,25 @@ function checkApiKey(req, res, next) {
     next(boom.unauthorized());
   }
 }
+function checkAdminRole(req, res, next) {
+  const user = req.user;
+  if (user.role === 'admin') {
+    next();
+  } else {
+    next(boom.forbidden('Se requieren permisos de administrador'));
+  }
+}
 
-module.exports = { checkApiKey };
+function checkRoles(...roles) {
+  return (req, res, next)=>{
+    const user = req.user;
+    if (roles.includes(user.role)){
+      next();
+    }
+    else{
+      next(boom.forbidden("Se requieren permisos de administrador o vendedor"));
+    }
+  }
+}
+
+module.exports = { checkApiKey, checkAdminRole, checkRoles };
